47c6823dc910b7fa8275376a296e7711cf747609
arayamiguel <miguel.araya.9@gmail.com>
2026-06-18 07:28:29 -0400

    curso-8

---
+++ compute-engine/commands.sh
@@ -0,0 +1,5 @@
+gcloud compute instances list
+
+gcloud compute instances describe servidor-web-1
+
+python backup-vm.py --project-id gcp-cloud-engineer-course-01 --disk-name servidor-web-1 --snapshot-name snapshot-servidor-web-1 --zone us-central1-a
