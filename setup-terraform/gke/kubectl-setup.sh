63461a195b8cafba61a88321f3b0ed8791d04651
arayamiguel <miguel.araya.9@gmail.com>
2026-06-19 11:54:35 -0400

    curso-10

---
+++ setup-terraform/gke/kubectl-setup.sh
@@ -0,0 +1,9 @@
+gcloud components install gke-gcloud-auth-plugin
+
+gcloud container clusters get-credentials mi-cluster-gke --zone us-central1-a
+
+gcloud components install kubectl
+
+kubectl get nodes
+
+kubectl get namespaces
