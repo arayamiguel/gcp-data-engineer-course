d2814bbafad7ce372a39332810a1bffd06c360a4
arayamiguel <miguel.araya.9@gmail.com>
2026-06-19 17:29:22 -0400

    curso-16

---
+++ cloud-sql-create.sh
@@ -0,0 +1,8 @@
+gcloud sql instances create mi-instancia-sql \
+  --database-version=POSTGRES_14 \
+  --cpu=1 \
+  --memory=4GB \
+  --region=us-central1 \
+  --root-password=supersecreto
+
+# una sola línea
